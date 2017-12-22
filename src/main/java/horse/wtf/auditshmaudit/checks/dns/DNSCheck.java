c3b9ac8d470513c7b07ec435cb5dec385c0ca5a0
Lennart Koopmann <lennart@graylog.com>
2017-12-22 18:34:50 +0100

    DNS check
    closes #6

--- src/main/java/horse/wtf/auditshmaudit/checks/dns/DNSCheck.java
+++ src/main/java/horse/wtf/auditshmaudit/checks/dns/DNSCheck.java
@@ -17,6 +17,9 @@
 
 package horse.wtf.auditshmaudit.checks.dns;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import horse.wtf.auditshmaudit.Issue;
 import horse.wtf.auditshmaudit.checks.Check;
 import horse.wtf.auditshmaudit.configuration.Configuration;
@@ -46,40 +49,105 @@ public class DNSCheck extends Check {
         this.configuration = configuration;
     }
 
-    // TODO make DNS server selection work and test with mutliple test (changing between tests)
-
     @Override
     protected List<Issue> check() {
         try {
+            String dnsQuestion = configuration.getString(this, C_DNS_QUESTION);
+            List<String> expected = configuration.getListOfStrings(this, C_EXPECTED_ANSWER);
+
             Lookup lookup = new Lookup(
-                    configuration.getString(this, C_DNS_QUESTION),
+                    dnsQuestion,
                     Type.value(configuration.getString(this, C_DNS_QUESTION_TYPE).toUpperCase())
             );
             lookup.setResolver(new SimpleResolver(configuration.getString(this, C_DNS_SERVER)));
 
-            Record[] records = lookup.run();
+            Record[] lookupResult = lookup.run();
 
             if (!lookup.getErrorString().equals("successful")) { // meeehhhhh stone age libraries
+                if(lookup.getErrorString().equals("host not found")) {
+                    addIssue(new Issue(this, "Domain [{}] not found at all.", dnsQuestion));
+                    return issues();
+                }
+
+                if(lookup.getErrorString().equals("type not found") && (expected == null || expected.isEmpty())) {
+                    LOG.debug("Empty result for lookup [{}] and empty result expected. Not raising an issue.", getFullCheckIdentifier());
+                    return issues();
+                }
+
+                if(lookup.getErrorString().equals("type not found") && (expected != null && !expected.isEmpty())) {
+                    addIssue(new Issue(this, "Expected records but did not find any."));
+                    return issues();
+                }
+
                 throw new RuntimeException("DNS lookup failed with error: " + lookup.getErrorString());
             }
 
-            for (Record record : records) {
+            ImmutableList.Builder<String> recordsBuilder = new ImmutableList.Builder<>();
+            for (Record record : lookupResult) {
                 switch(record.getType()) {
                     case Type.MX:
                         MXRecord mx = (MXRecord) record;
-                        LOG.info(mx.getTarget().toString(false));
+                        recordsBuilder.add(mx.getTarget().toString(true));
                         break;
                     case Type.A:
                         ARecord a = (ARecord) record;
-                        LOG.info(a.getAddress().toString());
+                        recordsBuilder.add(a.getAddress().getHostAddress());
                         break;
+                    case Type.AAAA:
+                        AAAARecord aaaa = (AAAARecord) record;
+                        recordsBuilder.add(aaaa.getAddress().getHostAddress());
+                        break;
+                    case Type.CNAME:
+                        CNAMERecord cname = (CNAMERecord) record;
+                        recordsBuilder.add(cname.getTarget().toString(true));
+                        break;
+                    case Type.TXT:
+                        TXTRecord txt = (TXTRecord) record;
+                        recordsBuilder.add(stripTxtRecord(txt.rdataToString()));
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported DNS question type [" + record.getType() + "].");
                 }
             }
 
+            // Make it a set to get rid of possible duplicates.
+            ImmutableSet<String> records = ImmutableSet.copyOf(recordsBuilder.build());
+
+            // Check if there is records at all and raise an issue if there are some but we expected none.
+            if((expected == null || expected.isEmpty()) && !records.isEmpty()) {
+                addIssue(new Issue(this, "Expected no DNS records but found <{}>. The records are: [{}]",
+                        records.size(), Joiner.on(", ").join(records)));
+                return issues();
+            }
+
+            // Check if number of records is correct.
+            if (records.size() != expected.size()) {
+                addIssue(new Issue(this, "Expected <{}> DNS records but found <{}>. The records are: [{}], but I expected [{}].",
+                        expected.size(), records.size(), Joiner.on(", ").join(records), Joiner.on(", ").join(expected)));
+                return issues();
+            }
+
+            // Check that each expected record was found.
+            for (String expectedRecord : expected) {
+                if (!records.contains(expectedRecord)) {
+                    addIssue(new Issue(this, "Expected records [{}] but found [{}].",
+                            Joiner.on(", ").join(expected), Joiner.on(", ").join(records)));
+                    return issues();
+                }
+            }
+
+            return issues();
         } catch(Exception e) {
             throw new RuntimeException("Could not run DNS lookup.", e);
         }
-        return issues();
+    }
+
+    private String stripTxtRecord(String txt) {
+        if (txt.startsWith("\"") && txt.endsWith("\"")) {
+            return txt.subSequence(1, txt.length()-1).toString();
+        }
+
+        return txt;
     }
 
     @Override
@@ -97,8 +165,7 @@ public class DNSCheck extends Check {
         return configuration.isCheckConfigurationComplete(this, Arrays.asList(
                 C_DNS_SERVER,
                 C_DNS_QUESTION,
-                C_DNS_QUESTION_TYPE,
-                C_EXPECTED_ANSWER
+                C_DNS_QUESTION_TYPE
         ));
     }
 }
